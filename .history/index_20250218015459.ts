b2f848da8ab4f8d27bd609c5281e9a93529d60ae
Romeo Mihalcea <eu@ciokan.ro>
2025-02-18 02:11:27 +0200

    first commit

---
+++ .history/index_20250218015459.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+export type TResponse = {
+    is: boolean;
+}
+
+export function useVisitorQuery(
+    publicApiKey: string,
+    endpoint: string = 'https://main.check.visitorquery.com/'
+) {
+    const [visitorQuery, setVisitorQuery] = useState<TResponse>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(endpoint, {
+                    headers: { 'X-Api-Key': publicApiKey },
+                });
+                const data: TResponse = await response.json();
+                setVisitorQuery(data);
+            } catch (error) {
+                console.error(`Error fetching visitorquery data: ${error}`);
+            }
+        };
+
+        fetchData().catch(console.error);
+    }, [publicApiKey, endpoint]);
+
+    return visitorQuery;
+}
---
+++ index.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+export type TResponse = {
+    is: boolean;
+    confidence: number;
+}
+
+export function useVisitorQuery(
+    publicApiKey: string,
+    endpoint: string = 'https://main.check.visitorquery.com/'
+) {
+    const [visitorQuery, setVisitorQuery] = useState<TResponse>({
+        is: false,
+        confidence: 0,
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(endpoint, {
+                    headers: { 'X-Api-Key': publicApiKey },
+                });
+                const data: TResponse = await response.json();
+                setVisitorQuery(data);
+            } catch (error) {
+                console.error(`Error fetching visitorquery data: ${error}`);
+            }
+        };
+
+        fetchData().catch(console.error);
+    }, [publicApiKey, endpoint]);
+
+    return visitorQuery;
+}
