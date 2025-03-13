1da1cd725c6d87d06fb859bff3a4233a22a6c468
Romeo Mihalcea <eu@ciokan.ro>
2025-03-13 14:24:43 +0200

    feat: added events

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import {useScript} from "./hooks.ts";
 
 declare global {
@@ -13,10 +13,21 @@ type Params = {
 	Endpoint?: string;
 }
 
+type State = {
+	Started: boolean;
+	Ended: boolean;
+	Errored?: boolean;
+}
+
 export function useVisitorQuery(p: Params) {
 	const defaultEndpoint = 'main.check.visitorquery.com';
 	const clientScript = useScript('https://cdn.visitorquery.com/visitorquery.js', {
 		removeOnUnmount: false,
+	});
+
+	const [state, setState] = useState<State>({
+		Started: false,
+		Ended  : false,
 	});
 
 	useEffect(() => {
@@ -24,8 +35,30 @@ export function useVisitorQuery(p: Params) {
 			window.VisitorQuery.run({
 				ApiKey   : p.ApiKey,
 				Endpoint : p.Endpoint || defaultEndpoint,
-				SessionId: p.SessionId
+				SessionId: p.SessionId,
+				onOpen   : () => {
+					setState({
+						Started: true,
+						Ended  : false,
+					});
+				},
+				onClose  : () => {
+					setState({
+						Started: true,
+						Ended  : true,
+					});
+				},
+				onError  : (err: Event) => {
+					setState({
+						Started: true,
+						Ended  : true,
+						Errored: true,
+					});
+					console.error(err);
+				}
 			});
 		}
 	}, [p, clientScript]);
+
+	return state;
 }
